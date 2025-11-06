ff85d48d0c78bad794d093a8bf0115db9d2f41e0
ChadMac6 <chadmackinlay@gmail.com>
2025-11-06 01:05:29 +0000

    additional world

--- 41068_ignition_bringup/launch/41068_ignition_drone.launch.py
+++ 41068_ignition_bringup/launch/41068_ignition_drone.launch.py
@@ -70,7 +70,7 @@ def generate_launch_description():
         'world',
         default_value='simple_trees',
         description='Which world to load',
-        choices=['simple_trees', 'large_demo', 'PlantationTest', 'Plantation2']
+        choices=['simple_trees', 'large_demo', 'PlantationTest', 'Plantation2', 'Plantation2Obs']
     )
     ld.add_action(world_launch_arg)
     gazebo = IncludeLaunchDescription(
